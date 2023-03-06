51f4435eade5fcff08b5dc01612148cb32b137ed
Novian Rachman Iskandar <novian19@upi.edu>
2023-03-06 12:52:48 +0700

    add navigation map

--- app/src/main/java/id/novian/flowablecash/view/MainActivity.kt
+++ app/src/main/java/id/novian/flowablecash/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.novian.flowablecash
+package id.novian.flowablecash.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import id.novian.flowablecash.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
